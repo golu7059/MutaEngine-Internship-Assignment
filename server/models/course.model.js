ae9a9786798585a1afb7a5de1a8ded2f41213b14
golu Kumar <golukumarjaisidih@gmail.com>
2024-09-20 18:45:42 +0530

    created schemas

---
+++ server/models/course.model.js
@@ -0,0 +1,62 @@
+import { Schema, model } from "mongoose";
+
+const courseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required !"],
+      minLength: [3, "Title should be atleast of 3 characters !"],
+      maxLength: [60, "Title cann't be greather than 60 characters !"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required !"],
+      minLength: [3, "Description should be atleast of 3 characters !"],
+      maxLength: [120, "Description cann't be greather than 60 characters !"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Please define category !"],
+    },
+    thumbnail: {
+      public_id: {
+        type: String,
+        // required: true,
+      },
+      secure_url: {
+        type: String,
+        // required: true,
+      },
+    },
+    lectures: [
+      {
+        title: {
+          type: String,
+          description: String,
+          lecture: {
+            // thumbnail of video
+            public_id: String,
+            secure_url: String,
+          },
+        },
+      },
+    ],
+    noOfLectures: {
+      type: Number,
+      default: 0,
+    },
+    createdBy: {
+      type: String,
+      required: [true, "Created by is important !"],
+      default: "Unknown",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Course = model("Course", courseSchema);
+export default Course;
